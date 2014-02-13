52e95015365de0430c58c9919576ca765fbf4266
K-Kensuke <black1993raven@gmail.com>
2014-02-13 17:42:06 +0900

    MovingAverageの仮実装

--- MotionAuth/src/com/example/motionauth/Lowpass/MovingAverage.java
+++ MotionAuth/src/com/example/motionauth/Lowpass/MovingAverage.java
@@ -6,8 +6,32 @@ package com.example.motionauth.Lowpass;
  */
 public class MovingAverage
 	{
-		public void LowpassFilter()
+		/**
+		 * Double型3次元配列データを受け取り，移動平均を用いて平滑化し，
+		 * 処理したDouble型3次元配列データを返す
+		 * @param data 平滑化を行うDouble型3次元配列データ
+		 * @return 平滑化を行ったDouble型3次元配列データ
+		 */
+		public double[][][] LowpassFilter(double[][][] data)
 			{
+				double outputData = 0.0;
 
+				for (int i = 0; i < 3; i++)
+					{
+						for (int j = 0; j < 3; i++)
+							{
+								for (int k = 0; k < 100; k++)
+									{
+										outputData = outputData * 0.9 + data[i][j][k] * 0.1;
+										data[i][j][k] = outputData;
+
+										if (k == 99)
+											{
+												outputData = 0.0;
+											}
+									}
+							}
+					}
+				return data;
 			}
 	}

9f10e70d9030f346303c1da4807f2a7268afc109
Kensuke Kosaka <k@trileg.net>
2016-07-22 18:12:35 +0900

    Refactoring Authentication: use static import

--- src/net/trileg/motionauth/Authentication/Authentication.java
+++ src/net/trileg/motionauth/Authentication/Authentication.java
@@ -5,19 +5,23 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import net.trileg.motionauth.R;
-import net.trileg.motionauth.Utility.Enum.STATUS;
 import net.trileg.motionauth.Utility.ListToArray;
-import net.trileg.motionauth.Utility.LogUtil;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+
+import static android.content.Intent.*;
+import static android.util.Log.*;
+import static android.view.MotionEvent.*;
+import static net.trileg.motionauth.Utility.Enum.STATUS.*;
+import static net.trileg.motionauth.Utility.LogUtil.log;
 
 /**
  * Authentication.
@@ -36,7 +40,7 @@ public class Authentication extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    LogUtil.log(Log.INFO);
+    log(INFO);
 
     setContentView(R.layout.activity_auth_motion);
     authentication();
@@ -47,7 +51,7 @@ public class Authentication extends Activity {
    * Registration event listener and call GetData using ExecutorService to collect data.
    */
   private void authentication() {
-    LogUtil.log(Log.INFO);
+    log(INFO);
 
     Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
     TextView nameTv = (TextView) findViewById(R.id.textView1);
@@ -56,27 +60,27 @@ public class Authentication extends Activity {
     getMotionBtn = (Button) findViewById(R.id.button1);
 
     nameTv.setText(InputName.userName + "さん読んでね！");
-    mGetData = new GetData(this, getMotionBtn, secondTv, vibrator, STATUS.UP);
+    mGetData = new GetData(this, getMotionBtn, secondTv, vibrator, UP);
 
     getMotionBtn.setOnTouchListener(new View.OnTouchListener() {
       @Override
       public boolean onTouch(View v, MotionEvent event) {
         switch (event.getAction()) {
-          case MotionEvent.ACTION_DOWN:
-            LogUtil.log(Log.VERBOSE, "Action down getMotionBtn");
+          case ACTION_DOWN:
+            log(VERBOSE, "Action down getMotionBtn");
             getMotionBtn.setText("取得中");
-            mGetData.changeStatus(STATUS.DOWN);
-            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            mGetData.changeStatus(DOWN);
+            ExecutorService executorService = newSingleThreadExecutor();
             executorService.execute(mGetData);
             executorService.shutdown();
             break;
-          case MotionEvent.ACTION_UP:
-            LogUtil.log(Log.VERBOSE, "Action up getMotionBtn");
-            mGetData.changeStatus(STATUS.UP);
+          case ACTION_UP:
+            log(VERBOSE, "Action up getMotionBtn");
+            mGetData.changeStatus(UP);
             break;
-          case MotionEvent.ACTION_CANCEL:
-            LogUtil.log(Log.VERBOSE, "Action cancel getMotionBtn");
-            mGetData.changeStatus(STATUS.UP);
+          case ACTION_CANCEL:
+            log(VERBOSE, "Action cancel getMotionBtn");
+            mGetData.changeStatus(UP);
             break;
         }
         return true;
@@ -90,16 +94,16 @@ public class Authentication extends Activity {
    * Call Result using ExecutorService to authenticate and show result.
    *
    * @param linearAccel Original linear acceleration data collecting from GetData.
-   * @param gyro  Original gyroscope data collecting from GetData.
+   * @param gyro        Original gyroscope data collecting from GetData.
    */
   void finishGetMotion(ArrayList<ArrayList<Float>> linearAccel,
                        ArrayList<ArrayList<Float>> gyro) {
-    LogUtil.log(Log.INFO);
+    log(INFO);
     if (getMotionBtn.isClickable()) getMotionBtn.setClickable(false);
     secondTv.setText("0");
     getMotionBtn.setText("認証処理中");
 
-    LogUtil.log(Log.DEBUG, "Start initialize ProgressDialog");
+    log(DEBUG, "Start initialize ProgressDialog");
 
     ProgressDialog progressDialog = new ProgressDialog(this);
     progressDialog.setTitle("計算処理中");
@@ -108,14 +112,14 @@ public class Authentication extends Activity {
     progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
     progressDialog.setCancelable(false);
 
-    LogUtil.log(Log.DEBUG, "Finish initialize ProgressDialog");
+    log(DEBUG, "Finish initialize ProgressDialog");
 
     progressDialog.show();
 
 
-    Result mResult = new Result(this, mListToArray.listTo2DArray(linearAccel), mListToArray.listTo2DArray(gyro),
-        getMotionBtn, progressDialog, mGetData);
-    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    Result mResult = new Result(this, mListToArray.listTo2DArray(linearAccel),
+        mListToArray.listTo2DArray(gyro), getMotionBtn, progressDialog, mGetData);
+    ExecutorService executorService = newSingleThreadExecutor();
     executorService.execute(mResult);
     executorService.shutdown();
   }
@@ -125,10 +129,10 @@ public class Authentication extends Activity {
    * Move to Start activity.
    */
   void finishAuthentication() {
-    LogUtil.log(Log.INFO);
+    log(INFO);
     Intent intent = new Intent();
     intent.setClassName(getPackageName(), getPackageName() + ".Start");
-    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+    intent.setFlags(FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_NEW_TASK);
 
     startActivity(intent);
     finish();

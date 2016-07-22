ce58cb4ff89f44e9d3aa7294652016672dbc57d5
Kensuke Kosaka <k@trileg.net>
2016-07-22 20:27:51 +0900

    Refactoring Amplifier: use static import

--- src/net/trileg/motionauth/Processing/Amplifier.java
+++ src/net/trileg/motionauth/Processing/Amplifier.java
@@ -1,8 +1,8 @@
 package net.trileg.motionauth.Processing;
 
-import android.util.Log;
-import net.trileg.motionauth.Utility.Enum;
-import net.trileg.motionauth.Utility.LogUtil;
+import static android.util.Log.*;
+import static net.trileg.motionauth.Utility.Enum.*;
+import static net.trileg.motionauth.Utility.LogUtil.log;
 
 /**
  * Amplify data.
@@ -20,23 +20,23 @@ public class Amplifier {
    * @return true if data is less than threshold even once during the entire number of trials, otherwise false.
    */
   public boolean CheckValueRange(double[][][] data, double checkRangeValue) {
-    LogUtil.log(Log.INFO);
+    log(INFO);
 
-    LogUtil.log(Log.DEBUG, "checkRangeValue" + checkRangeValue);
+    log(DEBUG, "checkRangeValue" + checkRangeValue);
 
-    double[][] max = new double[Enum.NUM_TIME][Enum.NUM_AXIS];
-    double[][] min = new double[Enum.NUM_TIME][Enum.NUM_AXIS];
+    double[][] max = new double[NUM_TIME][NUM_AXIS];
+    double[][] min = new double[NUM_TIME][NUM_AXIS];
 
-    for (int time = 0; time < Enum.NUM_TIME; time++) {
-      for (int axis = 0; axis < Enum.NUM_AXIS; axis++) {
+    for (int time = 0; time < NUM_TIME; time++) {
+      for (int axis = 0; axis < NUM_AXIS; axis++) {
         max[time][axis] = 0;
         min[time][axis] = 0;
       }
     }
 
     double range;
-    for (int time = 0; time < Enum.NUM_TIME; time++) {
-      for (int axis = 0; axis < Enum.NUM_AXIS; axis++) {
+    for (int time = 0; time < NUM_TIME; time++) {
+      for (int axis = 0; axis < NUM_AXIS; axis++) {
         for (int item = 0; item < data[time][axis].length; item++) {
           if (data[time][axis][item] > max[time][axis]) {
             max[time][axis] = data[time][axis][item];
@@ -47,10 +47,10 @@ public class Amplifier {
       }
     }
 
-    for (int time = 0; time < Enum.NUM_TIME; time++) {
-      for (int axis = 0; axis < Enum.NUM_AXIS; axis++) {
+    for (int time = 0; time < NUM_TIME; time++) {
+      for (int axis = 0; axis < NUM_AXIS; axis++) {
         range = max[time][axis] - min[time][axis];
-        LogUtil.log(Log.DEBUG, "range = " + range);
+        log(DEBUG, "range = " + range);
         if (range < checkRangeValue) isRangeCheck = true;
       }
     }
@@ -67,11 +67,11 @@ public class Amplifier {
    * @return Amplified double type 3-array data
    */
   public double[][][] Amplify(double[][][] data, double ampValue) {
-    LogUtil.log(Log.INFO);
+    log(INFO);
 
     if (ampValue != 0.0) {
-      for (int time = 0; time < Enum.NUM_TIME; time++) {
-        for (int axis = 0; axis < Enum.NUM_AXIS; axis++) {
+      for (int time = 0; time < NUM_TIME; time++) {
+        for (int axis = 0; axis < NUM_AXIS; axis++) {
           for (int item = 0; item < data[time][axis].length; item++) {
             data[time][axis][item] *= ampValue;
           }
@@ -90,10 +90,10 @@ public class Amplifier {
    * @return Amplified double type 2-array data
    */
   public double[][] Amplify(double[][] data, double ampValue) {
-    LogUtil.log(Log.INFO);
+    log(INFO);
 
     if (ampValue != 0.0) {
-      for (int axis = 0; axis < Enum.NUM_AXIS; axis++) {
+      for (int axis = 0; axis < NUM_AXIS; axis++) {
         for (int item = 0; item < data[axis].length; item++) {
           data[axis][item] *= ampValue;
         }
